05e319bfec0cee2dc0c07f0e0a43bb384f119336
Mike Mahoney <mike.mahoney.218@gmail.com>
2023-08-29 09:05:52 -0400

    Print, subset, test

--- R/misc.R
+++ R/misc.R
@@ -2,6 +2,37 @@
   ifelse(is.null(lhs), rhs, lhs)
 }
 
-# ## TODO
-# `[.rsi_band_mapping` <- `[`
-# `print.rsi_band_mapping` <- print
+#' @export
+`[.rsi_band_mapping` <- function(x, i) {
+  out <- x
+  attributes(out) <- NULL
+  names(out) <- names(x)
+  out <- out[i]
+  out_names <- names(out)
+  mostattributes(out) <- attributes(x)
+  names(out) <- out_names
+  out
+}
+
+#' @export
+`[[.rsi_band_mapping` <- function(x, i) {
+  out <- x
+  attributes(out) <- NULL
+  names(out) <- names(x)
+  out <- out[i]
+  out_names <- names(out)
+  mostattributes(out) <- attributes(x)
+  names(out) <- out_names
+  out
+}
+
+#' @export
+print.rsi_band_mapping <- function(x, ...) {
+  cat("An rsi band mapping object with attributes:\n")
+  cat(names(attributes(x)), sep = " ")
+  cat("\n\n")
+  x_names <- names(x)
+  attributes(x) <- NULL
+  names(x) <- x_names
+  print(x)
+}
--- tests/testthat/test-get_stac_data.R
+++ tests/testthat/test-get_stac_data.R
@@ -95,3 +95,29 @@ test_that("get_dem() is stable", {
     )
   )
 })
+
+test_that("non-default mappings work", {
+  skip_on_cran()
+  aoi <- sf::st_point(c(-74.912131, 44.080410)) |>
+    sf::st_sfc() |>
+    sf::st_set_crs(4326) |>
+    sf::st_transform(3857) |>
+    sf::st_buffer(1000)
+
+  expect_no_error(
+    out <- get_sentinel2_imagery(
+      aoi,
+      "2022-06-01",
+      "2022-06-30",
+      output_filename = tempfile(fileext = ".tif"),
+      asset_names = sentinel2_band_mapping$aws_v1
+    )
+  )
+  expect_no_error(terra::rast(out))
+  expect_true(
+    all(
+      names(terra::rast(out)) %in%
+        as.vector(sentinel2_band_mapping$aws_v1)
+    )
+  )
+})
