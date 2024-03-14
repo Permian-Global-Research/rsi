a103cd8d254eda3cd52bfb6d81e2c7e820925bb4
Hugh Graham <hugh.a.graham@gmail.com>
2024-03-14 16:48:10 +0000

    get_alos_palsar_imagery added to get_stac_data

--- R/get_stac_data.R
+++ R/get_stac_data.R
@@ -609,6 +609,51 @@ get_naip_imagery <- function(aoi,
 
 #' @rdname get_stac_data
 #' @export
+get_alos_palsar_imagery <- function(aoi,
+                                    start_date,
+                                    end_date,
+                                    ...,
+                                    pixel_x_size = 10,
+                                    pixel_y_size = 10,
+                                    asset_names = rsi::alos_palsar_band_mapping$planetary_computer_v1,
+                                    stac_source = attr(asset_names, "stac_source"),
+                                    collection = attr(asset_names, "collection_name"),
+                                    query_function = attr(asset_names, "query_function"),
+                                    sign_function = attr(asset_names, "sign_function"),
+                                    rescale_bands = FALSE,
+                                    item_filter_function = NULL,
+                                    mask_band = attr(asset_names, "mask_band"),
+                                    mask_function = attr(asset_names, "mask_function"),
+                                    output_filename = paste0(proceduralnames::make_english_names(1), ".tif"),
+                                    composite_function = "median",
+                                    limit = 999,
+                                    gdalwarp_options = c(
+                                      "-r", "bilinear",
+                                      "-multi",
+                                      "-overwrite",
+                                      "-co", "COMPRESS=DEFLATE",
+                                      "-co", "PREDICTOR=2",
+                                      "-co", "NUM_THREADS=ALL_CPUS"
+                                    ),
+                                    gdal_config_options = c(
+                                      VSI_CACHE = "TRUE",
+                                      GDAL_CACHEMAX = "30%",
+                                      VSI_CACHE_SIZE = "10000000",
+                                      GDAL_HTTP_MULTIPLEX = "YES",
+                                      GDAL_INGESTED_BYTES_AT_OPEN = "32000",
+                                      GDAL_DISABLE_READDIR_ON_OPEN = "EMPTY_DIR",
+                                      GDAL_HTTP_VERSION = "2",
+                                      GDAL_HTTP_MERGE_CONSECUTIVE_RANGES = "YES",
+                                      GDAL_NUM_THREADS = "ALL_CPUS"
+                                    )) {
+  args <- mget(names(formals()))
+  args$`...` <- NULL
+  args <- c(args, rlang::list2(...))
+  do.call(get_stac_data, args)
+}
+
+#' @rdname get_stac_data
+#' @export
 get_dem <- function(aoi,
                     ...,
                     start_date = NULL,
